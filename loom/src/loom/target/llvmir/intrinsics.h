df0ab120eab38303983978ef315ee095caed8262
Ben Vanik <ben.vanik@gmail.com>
2026-04-14 22:32:37 -0700

    Split LLVM intrinsic helpers by target

--- loom/src/loom/target/llvmir/intrinsics.h
+++ loom/src/loom/target/llvmir/intrinsics.h
@@ -9,21 +9,6 @@
 #ifndef LOOM_TARGET_LLVMIR_INTRINSICS_H_
 #define LOOM_TARGET_LLVMIR_INTRINSICS_H_
 
-#include "loom/target/llvmir/module.h"
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-iree_status_t loom_llvmir_declare_amdgcn_workitem_id_x(
-    loom_llvmir_module_t* module, loom_llvmir_function_t** out_function);
-
-iree_status_t loom_llvmir_declare_amdgcn_make_buffer_rsrc(
-    loom_llvmir_module_t* module, uint32_t result_address_space,
-    uint32_t base_address_space, loom_llvmir_function_t** out_function);
-
-#ifdef __cplusplus
-}
-#endif
+#include "loom/target/llvmir/intrinsics_amdgpu.h"
 
 #endif  // LOOM_TARGET_LLVMIR_INTRINSICS_H_
--- loom/src/loom/target/llvmir/intrinsics_amdgpu.c
+++ loom/src/loom/target/llvmir/intrinsics_amdgpu.c
@@ -4,7 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include "loom/target/llvmir/intrinsics.h"
+#include "loom/target/llvmir/intrinsics_amdgpu.h"
 
 #include <stdio.h>
 
