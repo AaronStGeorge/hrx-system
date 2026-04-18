6ff56eafbebacf14ce3c9e757754ba761c0497ae
Ben Vanik <ben.vanik@gmail.com>
2026-04-17 20:44:50 -0700

    Split AMDGPU descriptor shard target

--- loom/src/loom/target/arch/amdgpu/gfx950_descriptors_test.cc
+++ loom/src/loom/target/arch/amdgpu/gfx950_descriptors_test.cc
@@ -4,7 +4,7 @@
 // See https://llvm.org/LICENSE.txt for license information.
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-#include "loom/target/arch/amdgpu/descriptors.h"
+#include "loom/target/arch/amdgpu/gfx950_descriptors.h"
 
 #include <string>
 
